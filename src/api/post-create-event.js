a83c8754371124c04490a3a2a3f5948137135521
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-22 18:36:27 +0800

    all get and post api files for login, users and events. UNTESTED.

---
+++ src/api/post-create-event.js
@@ -0,0 +1,27 @@
+async function postCreateEvent(eventDetails) {
+  const url = `${import.meta.env.VITE_API_URL}/festivals/`;
+  const token = window.localStorage.getItem("token");
+
+  const newEvent = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+    body: JSON.stringify({ ...eventDetails }),
+  });
+
+  if (!newEvent.ok) {
+    const fallbackError = "Error trying to create new event";
+
+    const data = await newEvent.json().catch(() => {
+      throw new Error(fallbackError);
+    });
+
+    const errorMessage = data?.detail ?? fallbackError;
+    throw new Error(errorMessage);
+  }
+  return await newEvent.json();
+}
+
+export default postCreateEvent;
