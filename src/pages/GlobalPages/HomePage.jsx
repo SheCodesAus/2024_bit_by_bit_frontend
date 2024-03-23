5ac36bf2764529c11ce54fbbc7d21bebc0ee3c22
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-23 18:38:56 +0800

    adding button to test

--- src/pages/GlobalPages/HomePage.jsx
+++ src/pages/GlobalPages/HomePage.jsx
@@ -1,14 +1,24 @@
+// HOOKS
+import { useNavigate } from "react-router-dom";
+
 // ELEMENTS
 import GlobalAlertModal from "../../components/GlobalElements/GlobalAlertModal";
-
-// API
-import getEvent from "../../api/get-event";
+import ButtonElement from "../../components/GlobalElements/Button";
 
 function HomePage() {
+  const navigate = useNavigate();
+  // MODAL Variables
   const modalTitle = "Global Modal Template";
   const modalMessage = "This is a global modal template";
   const modalBtnTxt = "Modal";
   const modalAPICall = console.log("this is the api call");
+
+  //BUTTON Functions
+  const goToLoginPage = (event) => {
+    event.preventDefault();
+    console.log("going to login page");
+    navigate("/");
+  };
 
   return (
     <main>
@@ -19,6 +29,7 @@ function HomePage() {
         btnTxt={modalBtnTxt}
         btnAPICall={modalAPICall}
       />
+      <ButtonElement message="test button" btnClick={goToLoginPage} />
     </main>
   );
 }
