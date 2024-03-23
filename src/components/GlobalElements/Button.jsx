a70af22d1a7725da147304d786d70994dcb90b42
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-23 18:38:46 +0800

    removing excess styling (just colour for testing) + breaking down props.

--- src/components/GlobalElements/Button.jsx
+++ src/components/GlobalElements/Button.jsx
@@ -1,14 +1,14 @@
-function buttonElement(props) {
+function buttonElement({ btnClick, message }) {
   return (
-    <div className="mt-6 flex items-center justify-center gap-x-6">
+    <>
       <button
         type="submit"
-        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={props.btnClick}
+        className="rounded-md bg-orange-500"
+        onClick={btnClick}
       >
-        {props.message}
+        {message}
       </button>
-    </div>
+    </>
   );
 }
 
