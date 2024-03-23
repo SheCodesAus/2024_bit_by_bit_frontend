f3ec8a8e2f9f2c6da4f63314a3d001cb08af7960
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-23 18:06:26 +0800

    adding global modal, using home as a playground

--- src/pages/GlobalPages/HomePage.jsx
+++ src/pages/GlobalPages/HomePage.jsx
@@ -1,6 +1,26 @@
-function HomePage() {
+// ELEMENTS
+import GlobalAlertModal from "../../components/GlobalElements/GlobalAlertModal";
 
-	return <h1>This is the login page</h1>;
+// API
+import getEvent from "../../api/get-event";
+
+function HomePage() {
+  const modalTitle = "Global Modal Template";
+  const modalMessage = "This is a global modal template";
+  const modalBtnTxt = "Modal";
+  const modalAPICall = console.log("this is the api call");
+
+  return (
+    <main>
+      <h1>This is the login page</h1>
+      <GlobalAlertModal
+        title={modalTitle}
+        message={modalMessage}
+        btnTxt={modalBtnTxt}
+        btnAPICall={modalAPICall}
+      />
+    </main>
+  );
 }
 
 export default HomePage;
