15b77eca53fa407f01cf8a6dfa993da25b4fc35c
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-23 10:40:26 +0800

    creating register user form - UNTESTED. API deployment required for testing.

--- src/pages/GlobalPages/RegisterPage.jsx
+++ src/pages/GlobalPages/RegisterPage.jsx
@@ -1,6 +1,12 @@
+// COMPONENTS
+import RegisterUserForm from "../../components/UserComponents/RegisterUserForm";
 function RegisterPage() {
-
-	return <h1>This is the login page</h1>;
+  return (
+    <main>
+      <h1>This is the Register User page</h1>
+      <RegisterUserForm />
+    </main>
+  );
 }
 
 export default RegisterPage;
