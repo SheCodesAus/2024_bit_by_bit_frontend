b11c9e502a3a65901299724828f726356e1c3b69
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-24 16:56:49 +0800

    crating two profile components and testing all to the max wtihout API. Working as expected! 🎉

--- src/pages/UserPages/ProfilePage.jsx
+++ src/pages/UserPages/ProfilePage.jsx
@@ -1,6 +1,15 @@
-function ProfilePage() {
+//COMPONENTS
+import ProfileOverview from "../../components/UserComponents/ProfileOverview";
+import ProfileInformation from "../../components/UserComponents/ProfileDetails";
 
-	return <h1>This is the login page</h1>;
+function ProfilePage() {
+  return (
+    <main>
+      {/* add section for profile photo - update buttons and image? */}
+      <ProfileOverview />
+      <ProfileInformation />
+    </main>
+  );
 }
 
 export default ProfilePage;
