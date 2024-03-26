2aa71a7d6caf9340dce6043102e0ad21262492d0
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-26 08:10:41 +0800

    adding mock user data

---
+++ src/mock_user_data.js
@@ -0,0 +1,136 @@
+export const mockUserData = [
+  {
+    id: "1",
+    first_name: "Ulises",
+    last_name: "Mitchelson",
+    email: "umitchelson0@photobucket.com",
+    username: "umitchelson0",
+    profilePic: "http://dummyimage.com/197x100.png/ff4444/ffffff",
+    contactNumber: "6441165622",
+    slack: "",
+    linkedIn:
+      "https://parallels.com/nam/dui/proin/leo/odio.js?velit=lacinia&id=aenean&pretium=sit&iaculis=amet&diam=justo&erat=morbi&fermentum=ut&justo=odio&nec=cras&condimentum=mi&neque=pede&sapien=malesuada&placerat=in&ante=imperdiet&nulla=et&justo=commodo&aliquam=vulputate&quis=justo&turpis=in&eget=blandit&elit=ultrices&sodales=enim&scelerisque=lorem&mauris=ipsum&sit=dolor&amet=sit&eros=amet&suspendisse=consectetuer&accumsan=adipiscing&tortor=elit&quis=proin&turpis=interdum&sed=mauris&ante=non&vivamus=ligula&tortor=pellentesque&duis=ultrices&mattis=phasellus&egestas=id",
+    bio: "Donec dapibus. Duis at velit eu est congue elementum.",
+  },
+  {
+    id: "2",
+    first_name: "Daune",
+    last_name: "Vanin",
+    email: "dvanin1@globo.com",
+    username: "dvanin1",
+    profilePic: "http://dummyimage.com/250x100.png/5fa2dd/ffffff",
+    contactNumber: "2212717102",
+    slack:
+      "http://topsy.com/tellus/semper.json?tempus=dui&vel=nec&pede=nisi&morbi=volutpat&porttitor=eleifend&lorem=donec&id=ut&ligula=dolor&suspendisse=morbi&ornare=vel&consequat=lectus&lectus=in&in=quam&est=fringilla&risus=rhoncus&auctor=mauris&sed=enim&tristique=leo&in=rhoncus&tempus=sed&sit=vestibulum&amet=sit&sem=amet&fusce=cursus&consequat=id&nulla=turpis&nisl=integer&nunc=aliquet&nisl=massa&duis=id&bibendum=lobortis&felis=convallis&sed=tortor&interdum=risus&venenatis=dapibus&turpis=augue&enim=vel&blandit=accumsan&mi=tellus&in=nisi&porttitor=eu&pede=orci&justo=mauris&eu=lacinia&massa=sapien&donec=quis&dapibus=libero&duis=nullam&at=sit&velit=amet&eu=turpis&est=elementum&congue=ligula&elementum=vehicula&in=consequat&hac=morbi&habitasse=a&platea=ipsum&dictumst=integer&morbi=a&vestibulum=nibh&velit=in&id=quis&pretium=justo&iaculis=maecenas&diam=rhoncus&erat=aliquam&fermentum=lacus&justo=morbi&nec=quis&condimentum=tortor&neque=id&sapien=nulla&placerat=ultrices&ante=aliquet&nulla=maecenas&justo=leo&aliquam=odio&quis=condimentum&turpis=id&eget=luctus&elit=nec&sodales=molestie&scelerisque=sed&mauris=justo&sit=pellentesque&amet=viverra&eros=pede&suspendisse=ac&accumsan=diam&tortor=cras&quis=pellentesque&turpis=volutpat&sed=dui&ante=maecenas",
+    linkedIn:
+      "https://omniture.com/nam/congue/risus/semper/porta/volutpat/quam.js?nibh=sapien&in=iaculis&quis=congue&justo=vivamus&maecenas=metus&rhoncus=arcu&aliquam=adipiscing&lacus=molestie&morbi=hendrerit&quis=at&tortor=vulputate&id=vitae&nulla=nisl&ultrices=aenean&aliquet=lectus&maecenas=pellentesque&leo=eget&odio=nunc&condimentum=donec&id=quis&luctus=orci&nec=eget&molestie=orci&sed=vehicula&justo=condimentum&pellentesque=curabitur&viverra=in&pede=libero&ac=ut&diam=massa&cras=volutpat&pellentesque=convallis&volutpat=morbi&dui=odio&maecenas=odio&tristique=elementum&est=eu&et=interdum&tempus=eu&semper=tincidunt&est=in&quam=leo&pharetra=maecenas",
+    bio: '"Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo',
+  },
+  {
+    id: "3",
+    first_name: "Bethanne",
+    last_name: "Lowle",
+    email: "blowle2@slideshare.net",
+    username: "blowle2",
+    profilePic: "",
+    contactNumber: "1614914752",
+    slack:
+      "https://youtu.be/quisque/erat/eros/viverra/eget.jsp?aenean=mattis&sit=pulvinar&amet=nulla&justo=pede&morbi=ullamcorper&ut=augue&odio=a&cras=suscipit&mi=nulla&pede=elit&malesuada=ac&in=nulla&imperdiet=sed&et=vel&commodo=enim&vulputate=sit&justo=amet&in=nunc&blandit=viverra&ultrices=dapibus&enim=nulla&lorem=suscipit&ipsum=ligula&dolor=in&sit=lacus&amet=curabitur&consectetuer=at&adipiscing=ipsum&elit=ac&proin=tellus&interdum=semper&mauris=interdum&non=mauris&ligula=ullamcorper&pellentesque=purus&ultrices=sit",
+    linkedIn:
+      "https://printfriendly.com/accumsan/odio/curabitur.json?donec=nullam&vitae=sit&nisi=amet&nam=turpis&ultrices=elementum&libero=ligula&non=vehicula&mattis=consequat&pulvinar=morbi&nulla=a&pede=ipsum&ullamcorper=integer&augue=a&a=nibh&suscipit=in&nulla=quis&elit=justo&ac=maecenas&nulla=rhoncus&sed=aliquam&vel=lacus&enim=morbi&sit=quis&amet=tortor&nunc=id&viverra=nulla&dapibus=ultrices&nulla=aliquet&suscipit=maecenas&ligula=leo&in=odio&lacus=condimentum&curabitur=id",
+    bio: '"Cum sociis natoque penatibus et magnis dis parturient montes',
+  },
+  {
+    id: "4",
+    first_name: "Bennie",
+    last_name: "Sute",
+    email: "bsute3@amazon.com",
+    username: "bsute3",
+    profilePic: "",
+    contactNumber: "4639764789",
+    slack: "",
+    linkedIn:
+      "https://arstechnica.com/fusce/congue/diam.json?quis=in&tortor=est&id=risus&nulla=auctor&ultrices=sed&aliquet=tristique&maecenas=in",
+    bio: '"Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio',
+  },
+  {
+    id: "5",
+    first_name: "Kristo",
+    last_name: "Jex",
+    email: "kjex4@dailymail.co.uk",
+    username: "kjex4",
+    profilePic: "http://dummyimage.com/242x100.png/cc0000/ffffff",
+    contactNumber: "5413673203",
+    slack: "",
+    linkedIn:
+      "http://nhs.uk/dolor/vel/est/donec.xml?commodo=penatibus&vulputate=et&justo=magnis&in=dis&blandit=parturient&ultrices=montes&enim=nascetur&lorem=ridiculus&ipsum=mus&dolor=etiam&sit=vel&amet=augue&consectetuer=vestibulum&adipiscing=rutrum&elit=rutrum&proin=neque&interdum=aenean&mauris=auctor&non=gravida&ligula=sem&pellentesque=praesent&ultrices=id&phasellus=massa&id=id&sapien=nisl&in=venenatis&sapien=lacinia&iaculis=aenean&congue=sit&vivamus=amet&metus=justo&arcu=morbi&adipiscing=ut&molestie=odio&hendrerit=cras&at=mi&vulputate=pede&vitae=malesuada&nisl=in&aenean=imperdiet&lectus=et&pellentesque=commodo&eget=vulputate&nunc=justo&donec=in&quis=blandit&orci=ultrices&eget=enim&orci=lorem&vehicula=ipsum&condimentum=dolor&curabitur=sit&in=amet",
+    bio: '"Fusce lacus purus',
+  },
+  {
+    id: "6",
+    first_name: "Galen",
+    last_name: "Masdon",
+    email: "gmasdon5@godaddy.com",
+    username: "gmasdon5",
+    profilePic: "http://dummyimage.com/206x100.png/cc0000/ffffff",
+    contactNumber: "9489789181",
+    slack: "",
+    linkedIn:
+      "http://bluehost.com/velit/nec/nisi/vulputate/nonummy/maecenas.png?cras=purus&pellentesque=phasellus&volutpat=in&dui=felis&maecenas=donec&tristique=semper&est=sapien&et=a&tempus=libero&semper=nam&est=dui&quam=proin&pharetra=leo&magna=odio&ac=porttitor&consequat=id&metus=consequat&sapien=in&ut=consequat&nunc=ut&vestibulum=nulla&ante=sed&ipsum=accumsan&primis=felis&in=ut",
+    bio: '"Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo',
+  },
+  {
+    id: "7",
+    first_name: "Cherianne",
+    last_name: "Sollars",
+    email: "csollars6@eepurl.com",
+    username: "csollars6",
+    profilePic: "http://dummyimage.com/156x100.png/5fa2dd/ffffff",
+    contactNumber: "2567146169",
+    slack:
+      "http://narod.ru/luctus/et/ultrices/posuere/cubilia.xml?et=tincidunt&ultrices=eu&posuere=felis&cubilia=fusce&curae=posuere&donec=felis&pharetra=sed&magna=lacus&vestibulum=morbi&aliquet=sem&ultrices=mauris&erat=laoreet&tortor=ut&sollicitudin=rhoncus&mi=aliquet&sit=pulvinar&amet=sed",
+    linkedIn:
+      "http://gov.uk/rhoncus/sed/vestibulum.png?a=sed&pede=tincidunt&posuere=eu&nonummy=felis&integer=fusce&non=posuere&velit=felis&donec=sed&diam=lacus&neque=morbi&vestibulum=sem&eget=mauris&vulputate=laoreet&ut=ut&ultrices=rhoncus&vel=aliquet&augue=pulvinar&vestibulum=sed&ante=nisl&ipsum=nunc&primis=rhoncus&in=dui&faucibus=vel&orci=sem&luctus=sed&et=sagittis&ultrices=nam&posuere=congue&cubilia=risus&curae=semper&donec=porta&pharetra=volutpat&magna=quam&vestibulum=pede&aliquet=lobortis",
+    bio: '"Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes',
+  },
+  {
+    id: "8",
+    first_name: "Emmy",
+    last_name: "Garey",
+    email: "egarey7@mit.edu",
+    username: "egarey7",
+    profilePic: "http://dummyimage.com/198x100.png/ff4444/ffffff",
+    contactNumber: "1874739097",
+    slack:
+      "https://princeton.edu/turpis/donec.jsp?dapibus=eget&dolor=congue&vel=eget&est=semper&donec=rutrum&odio=nulla&justo=nunc&sollicitudin=purus&ut=phasellus&suscipit=in&a=felis&feugiat=donec&et=semper&eros=sapien&vestibulum=a&ac=libero&est=nam&lacinia=dui&nisi=proin&venenatis=leo&tristique=odio&fusce=porttitor&congue=id&diam=consequat&id=in&ornare=consequat&imperdiet=ut&sapien=nulla&urna=sed&pretium=accumsan&nisl=felis&ut=ut&volutpat=at&sapien=dolor&arcu=quis&sed=odio&augue=consequat",
+    linkedIn:
+      "http://intel.com/dignissim/vestibulum/vestibulum/ante.png?sit=interdum&amet=mauris&cursus=non&id=ligula&turpis=pellentesque&integer=ultrices&aliquet=phasellus&massa=id&id=sapien&lobortis=in&convallis=sapien&tortor=iaculis&risus=congue&dapibus=vivamus&augue=metus&vel=arcu&accumsan=adipiscing&tellus=molestie&nisi=hendrerit&eu=at&orci=vulputate&mauris=vitae&lacinia=nisl&sapien=aenean&quis=lectus&libero=pellentesque&nullam=eget&sit=nunc&amet=donec&turpis=quis&elementum=orci&ligula=eget&vehicula=orci&consequat=vehicula&morbi=condimentum&a=curabitur&ipsum=in&integer=libero&a=ut&nibh=massa&in=volutpat&quis=convallis&justo=morbi&maecenas=odio&rhoncus=odio&aliquam=elementum&lacus=eu&morbi=interdum&quis=eu&tortor=tincidunt&id=in&nulla=leo&ultrices=maecenas&aliquet=pulvinar&maecenas=lobortis&leo=est&odio=phasellus&condimentum=sit&id=amet&luctus=erat&nec=nulla&molestie=tempus&sed=vivamus&justo=in&pellentesque=felis&viverra=eu&pede=sapien&ac=cursus&diam=vestibulum&cras=proin&pellentesque=eu&volutpat=mi&dui=nulla&maecenas=ac&tristique=enim",
+    bio: '"Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero',
+  },
+  {
+    id: "9",
+    first_name: "Spenser",
+    last_name: "Ebbitt",
+    email: "sebbitt8@weather.com",
+    username: "sebbitt8",
+    profilePic: "",
+    contactNumber: "8586419578",
+    slack: "",
+    linkedIn:
+      "https://craigslist.org/mauris/laoreet/ut/rhoncus/aliquet/pulvinar/sed.xml?sem=morbi&mauris=odio&laoreet=odio&ut=elementum&rhoncus=eu&aliquet=interdum&pulvinar=eu&sed=tincidunt&nisl=in&nunc=leo&rhoncus=maecenas&dui=pulvinar&vel=lobortis&sem=est&sed=phasellus&sagittis=sit&nam=amet&congue=erat&risus=nulla&semper=tempus&porta=vivamus",
+    bio: '"Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam',
+  },
+  {
+    id: "10",
+    first_name: "Joella",
+    last_name: "Sear",
+    email: "jsear9@google.pl",
+    username: "jsear9",
+    profilePic: "http://dummyimage.com/177x100.png/5fa2dd/ffffff",
+    contactNumber: "1219542577",
+    slack:
+      "https://feedburner.com/adipiscing/elit/proin/interdum/mauris/non.json?urna=fusce&pretium=posuere&nisl=felis&ut=sed&volutpat=lacus&sapien=morbi&arcu=sem&sed=mauris&augue=laoreet&aliquam=ut&erat=rhoncus&volutpat=aliquet&in=pulvinar&congue=sed&etiam=nisl&justo=nunc&etiam=rhoncus&pretium=dui&iaculis=vel&justo=sem&in=sed&hac=sagittis&habitasse=nam&platea=congue&dictumst=risus&etiam=semper&faucibus=porta&cursus=volutpat&urna=quam&ut=pede&tellus=lobortis",
+    linkedIn: "",
+    bio: '"In tempor',
+  },
+];
