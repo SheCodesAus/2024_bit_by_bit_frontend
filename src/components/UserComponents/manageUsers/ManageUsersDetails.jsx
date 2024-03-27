47988e4a55e764b3b1b06fc2f65ca9ba553125c3
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-27 10:30:02 +0800

    next stages on manage user details - not complete. need to get the second half of the accordion map working.

--- src/components/UserComponents/manageUsers/ManageUsersDetails.jsx
+++ src/components/UserComponents/manageUsers/ManageUsersDetails.jsx
@@ -1,100 +1,83 @@
 // HOOKS
 import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { useAuth } from "../../../hooks/use-auth";
+// import { useParams } from "react-router-dom";
+// import { useAuth } from "../../../hooks/use-auth";
 
-// DUMMY DATA
-import { mockUserData } from "../../../mock_user_data";
+// // DUMMY DATA
+// import { mockUserData } from "../../../mock_user_data";
 // import { mockUserProcessData } from "../../mock_user_process_data";
 
 // https://www.youtube.com/watch?v=IW_AYg3kUIY
 // https://www.sitepoint.com/react-js-accordion-component/
 // https://www.freecodecamp.org/news/build-accordion-menu-in-react-without-external-libraries/
 
-function ManageUserDetails() {
-  const items = document.querySelectorAll("#accordion .item .header");
-  items.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      e.target.closest(".item").classList.toggle("active");
-    });
-  });
+function ManageUserDetails(item) {
+  console.log(item);
+  const [isActive, setIsActive] = useState(false);
 
   return (
-    <>
-      <section className="flex justify-center items-center h-screen bg-slate-600 text-white p-5">
-        <div
-          id="accordion"
-          className="w-[50%] rounded-lg overflow-hidden bg-slate-800 flex flex-col gap-[1px]"
+    <section className="accordion bg-slate-200">
+      <div className="accordian-item">
+        <tbody
+          className="accordian-header bg-slate-500"
+          onClick={() => setIsActive(!isActive)}
         >
-          <div className="item active">
-            <div className="header p-6 bg-slate-900 font-bold flex justify-between items-center cursor-pointer">
-              <h2>HEADER</h2>
-              <p>Subheading</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 activeIcon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 inactiveIcon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 15.75 7.5-7.5 7.5 7.5"
-                />
-              </svg>
-            </div>
-            <div className="content text-xl text-grey-300 transition-all duration-500">
-              <p className="mb-5">THIS IS TEXT, PARA TEXT.</p>
-            </div>
-          </div>
-        </div>
-
-        <br />
-      </section>
-      <table className="w=[50%] ">
-        <thead>
           <tr>
-            <th>First Name</th>
-            <th>Second Name</th>
-            <th>Email</th>
-            <th>Contact Number</th>
-            <th>Slack</th>
-            <th>LinkedIn</th>
+            <td>{item.item.first_name}</td>
+            <td>{item.item.last_name}</td>
+            <td>{item.item.email}</td>
+            <td>{item.item.contactNumber}</td>
+            <td>{item.item.slack}</td>
+            <td>{item.item.linkedIn}</td>
           </tr>
-        </thead>
-        {mockUserData.map((userData, key) => {
-          return (
-            <tbody key={key}>
+          <div>{isActive ? "-" : "+"}</div>
+
+          {isActive && (
+            {item.item.user_process((process, index)=>(
+            <div className="accordian-content" key={index}>
               <tr>
-                <td>{userData.first_name}</td>
-                <td>{userData.last_name}</td>
-                <td>{userData.email}</td>
-                <td>{userData.contactNumber}</td>
-                <td>{userData.slack}</td>
-                <td>{userData.linkedIn}</td>
+                <td>{process.}</td>
+                <td>YES</td>
               </tr>
-            </tbody>
-          );
-        })}
-      </table>
-    </>
+            </div>
+            ))}
+          )}
+        </tbody>
+      </div>
+    </section>
   );
 }
+
+// {
+
+//         <br />
+//       </section>
+//       <table className="w=[50%] ">
+//         <thead>
+//           <tr>
+//             <th>First Name</th>
+//             <th>Second Name</th>
+//             <th>Email</th>
+//             <th>Contact Number</th>
+//             <th>Slack</th>
+//             <th>LinkedIn</th>
+//           </tr>
+//         </thead>
+//         {mockUserData.map((userData, key) => {
+//           return (
+//             <tbody key={key}>
+//               <tr>
+//                 <td>{userData.first_name}</td>
+//                 <td>{userData.last_name}</td>
+//                 <td>{userData.email}</td>
+//                 <td>{userData.contactNumber}</td>
+//                 <td>{userData.slack}</td>
+//                 <td>{userData.linkedIn}</td>
+//               </tr>
+//             </tbody>
+//           );
+//         })}
+//       </table>
+// }
 
 export default ManageUserDetails;
--- src/pages/UserPages/ManageUsersPage.jsx
+++ src/pages/UserPages/ManageUsersPage.jsx
@@ -1,11 +1,28 @@
 // COMPONENTS
 import ManageUserDetails from "../../components/UserComponents/manageUsers/ManageUsersDetails";
 
+// DUMMY DATA
+import { mockUserData } from "../../mock_user_data";
+
 function ManageUsersPage() {
   return (
     <main>
       <h1>This is the Manage users page</h1>
-      <ManageUserDetails />
+      <table className="accordion">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Second Name</th>
+            <th>Email</th>
+            <th>Contact Number</th>
+            <th>Slack</th>
+            <th>LinkedIn</th>
+          </tr>
+        </thead>
+        {mockUserData.map((item, index) => (
+          <ManageUserDetails item={item} key={index} />
+        ))}
+      </table>
     </main>
   );
 }
