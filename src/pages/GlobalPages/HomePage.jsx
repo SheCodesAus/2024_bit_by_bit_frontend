a0731d9f9e91ac763002bb8b31c52dc289f6bf98
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-28 08:31:35 +0800

    resolving homepage bug - (useProjects)

--- src/pages/GlobalPages/HomePage.jsx
+++ src/pages/GlobalPages/HomePage.jsx
@@ -1,43 +1,24 @@
 // HOOKS
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // ELEMENTS
-import GlobalAlertModal from "../../components/GlobalElements/GlobalAlertModal";
-import ButtonElement from "../../components/GlobalElements/Button";
 import { useNavbarContext } from "../../components/NavBarContext";
 
 function HomePage() {
-  const { projects } = useProjects();
-  const navigate = useNavigate();
-  // MODAL Variables
-  const modalTitle = "Global Modal Template";
-  const modalMessage = "This is a global modal template";
-  const modalBtnTxt = "Modal";
-  const modalAPICall = console.log("this is the api call");
-
-  //BUTTON Functions
-  const goToLoginPage = (event) => {
-    event.preventDefault();
-    console.log("going to login page");
-    navigate("/");
-  };
+  // const { projects } = useProjects();
+  // const navigate = useNavigate();
 
   const { isNavbarOpen } = useNavbarContext();
   const bannerPath = "/imgs/banner_img.jpg";
 
   return (
-    <main className={`min-h-screen ${isNavbarOpen ? "ml-60" : "ml-20"} justify-center`}>
-
-      {/* <GlobalAlertModal
-        title={modalTitle}
-        message={modalMessage}
-        btnTxt={modalBtnTxt}
-        btnAPICall={modalAPICall}
-      />
-      <ButtonElement message="test button" btnClick={goToLoginPage} /> */}
-
+    <main
+      className={`min-h-screen ${
+        isNavbarOpen ? "ml-60" : "ml-20"
+      } justify-center`}
+    >
       <section className="border-b p-4 border-gray-300">
-        <img src={bannerPath}/>
+        <img src={bannerPath} />
         <div className="flex justify-center border-b p-4 border-gray-300 mb-4">
           <h1 className="font-bold text-3xl">Welcome to BYTE TIME!</h1>
         </div>
@@ -49,14 +30,32 @@ function HomePage() {
         <h3> click on one of the programs below for more information </h3>
       </section>
       <section className="flex flex-row justify-center border-b p-8 border-gray-300 mb-4">
-        <div className="event-card bg-white shadow-md p-8 rounded-lg mx-4 flex justify-center border border-orange-300" style={{ minWidth: '300px' }}>
-          <Link to="/about" className="text-xl"> PLUS </Link>
+        <div
+          className="event-card bg-white shadow-md p-8 rounded-lg mx-4 flex justify-center border border-orange-300"
+          style={{ minWidth: "300px" }}
+        >
+          <Link to="/about" className="text-xl">
+            {" "}
+            PLUS{" "}
+          </Link>
         </div>
-        <div className="event-card bg-white shadow-md p-8 rounded-lg mx-4 flex justify-center border border-orange-300" style={{ minWidth: '300px' }}>
-        <Link to="/about" className="text-xl"> ONE-DAY WORKSHOPS </Link>
+        <div
+          className="event-card bg-white shadow-md p-8 rounded-lg mx-4 flex justify-center border border-orange-300"
+          style={{ minWidth: "300px" }}
+        >
+          <Link to="/about" className="text-xl">
+            {" "}
+            ONE-DAY WORKSHOPS{" "}
+          </Link>
         </div>
-        <div className="event-card bg-white shadow-md p-8 rounded-lg mx-4 flex justify-center border border-orange-300" style={{ minWidth: '300px' }}>
-        <Link to="/about" className="text-xl"> FLASH </Link>
+        <div
+          className="event-card bg-white shadow-md p-8 rounded-lg mx-4 flex justify-center border border-orange-300"
+          style={{ minWidth: "300px" }}
+        >
+          <Link to="/about" className="text-xl">
+            {" "}
+            FLASH{" "}
+          </Link>
         </div>
       </section>
     </main>
