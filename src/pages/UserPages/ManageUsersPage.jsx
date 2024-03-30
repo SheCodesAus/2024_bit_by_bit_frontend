05aca62d664fe28d023859d756e6cdfb8ed9a2ff
caoimhejyoti <caoimhejyoti@gmail.com>
2024-03-30 17:51:58 +0800

    getting the accordion to pull  data, layout better and have check boxes - testing required

--- src/pages/UserPages/ManageUsersPage.jsx
+++ src/pages/UserPages/ManageUsersPage.jsx
@@ -1,15 +1,23 @@
 // COMPONENTS
+import { useState, useEffect } from "react";
+
 import ManageUserDetails from "../../components/UserComponents/manageUsers/ManageUsersDetails";
+
+// API
+import useAllUsers from "../../hooks/use-all-users";
 
 // DUMMY DATA
 import { mockUserData } from "../../mock_user_data";
 
 function ManageUsersPage() {
+  const { users } = useAllUsers();
+  // const [isActive, setIsActive] = useState(false);
+
   return (
     <main>
       <h1>This is the Manage users page</h1>
       <table className="accordion">
-        <thead>
+        <thead className="accordian-item">
           <tr>
             <th>First Name</th>
             <th>Second Name</th>
@@ -17,8 +25,10 @@ function ManageUsersPage() {
             <th>Contact Number</th>
             <th>Slack</th>
             <th>LinkedIn</th>
+            <th></th>
           </tr>
         </thead>
+
         {mockUserData.map((item, index) => (
           <ManageUserDetails item={item} key={index} />
         ))}
