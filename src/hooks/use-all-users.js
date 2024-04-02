6d2819f709cf2682bb18671a236d6abc642c1d60
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-02 22:02:58 +0800

    removing auth from all users hook

--- src/hooks/use-all-users.js
+++ src/hooks/use-all-users.js
@@ -1,26 +1,21 @@
 import { useState, useEffect } from "react";
 import getUsers from "../api/get-users";
-import { useAuth } from "./use-auth";
 
 export default function useAllUsers() {
-  const { auth, setAuth } = useAuth();
-
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
 
   useEffect(() => {
-    if (auth) {
-      getUsers()
-        .then((users) => {
-          setUsers(users);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-          setIsLoading(false);
-        });
-    }
-  }, [auth]);
+    getUsers()
+      .then((users) => {
+        setUsers(users);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
   return { users, isLoading, error };
 }
