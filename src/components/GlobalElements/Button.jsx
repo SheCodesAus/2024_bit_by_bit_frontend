55ff5adbce5c32477e9bb334e8f7fd2baef4d351
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-03 18:07:09 +0800

    updating global button styling

--- src/components/GlobalElements/Button.jsx
+++ src/components/GlobalElements/Button.jsx
@@ -3,7 +3,7 @@ function buttonElement({ btnClick, message }) {
     <>
       <button
         type="submit"
-        className="rounded-md px-3 py-2 bg-orange-500"
+        className="inline-flex w-full justify-center rounded-md px-3 py-2 bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
         onClick={btnClick}
       >
         {message}
