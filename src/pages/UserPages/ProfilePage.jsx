4c8abca5211f858c78b314745e75c4423798c583
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-06 19:40:38 +0800

    removing all console logs and unnecessary code/removed code due to design changes

--- src/pages/UserPages/ProfilePage.jsx
+++ src/pages/UserPages/ProfilePage.jsx
@@ -1,29 +1,20 @@
 // HOOKS
 import { useEffect, useState } from "react";
-import useUser from "../../hooks/use-user.js";
-import { useAuth } from "../../hooks/use-auth.js";
 import { useParams } from "react-router-dom";
 
 //COMPONENTS
 import ProfileOverview from "../../components/UserComponents/ProfileOverview";
 import ProfileInformation from "../../components/UserComponents/ProfileDetails";
 import { useNavbarContext } from "../../components/NavBarContext";
-// import EventCard from "../../components/GlobalElements/EventCard";
 import ProfileEventCard from "../../components/UserComponents/Profile-EventCard.jsx";
-import { CheckBadgeIcon } from "@heroicons/react/24/solid";
 
 // API
 import getUser from "../../api/get-user.js";
 import getEvents from "../../api/get-events.js";
-import getUserProcess from "../../api/get-user-process.js";
 import getEventMentors from "../../api/get-event-mentors.js";
 
 function ProfilePage() {
-  const { auth } = useAuth();
-  const { user } = useUser(auth);
-  const { token } = useUser(auth.token);
   const { id } = useParams();
-  console.log("user: ", user);
   const { isNavbarOpen } = useNavbarContext();
   const [selectedCity] = useState("");
 
@@ -38,17 +29,6 @@ function ProfilePage() {
         setUserError(error);
       });
   }, []);
-
-  // Example events data
-  const events = [
-    {
-      id: 1,
-      program: "Event 1",
-      date: "1/1/2024",
-      city: "brisbane",
-      time: "9am - 1pm",
-    },
-  ];
 
   // REGISTERED EVENTS
 
@@ -92,17 +72,12 @@ function ProfilePage() {
       }
     });
 
-  console.log("mentorApplications after push: ", mentorApplications);
-
   let eventApplications = [];
   mentorApplications.length > 0 &&
     eventData &&
     mentorApplications.map((mentor) => {
-      console.log("map step 1");
       eventData.map((event) => {
-        console.log("map step 2");
         if (mentor.event_id == event.id) {
-          console.log("map if fnc");
           const addEventApplication = {
             mentor_id: mentor.id,
             event_id: mentor.event_id,
@@ -118,46 +93,6 @@ function ProfilePage() {
         }
       });
     });
-
-  console.log("eventApplications after push: ", eventApplications);
-
-  const [userProcess, setUserProcess] = useState();
-  const [processError, setProcessError] = useState();
-
-  useEffect(() => {
-    getUserProcess(id, token)
-      .then((processes) => {
-        setUserProcess(processes);
-      })
-      .catch((error) => {
-        setProcessError(error);
-      });
-  }, [id, token]);
-  console.log("userProcess: ", userProcess);
-
-  const filteredEvents = selectedCity
-    ? events.filter(
-        (event) => event.location.toLowerCase() === selectedCity.toLowerCase()
-      )
-    : events;
-
-  // const userProcess = {
-  //   user_onboarding_task: {
-  //     "Slack provided": false,
-  //     "LinkedIn provided": true,
-  //     "Mentor code of conduct provided": false,
-  //     "Mentor t-shirt provided": false,
-  //   },
-  //   user_offboarding_task: {
-  //     "Feedback asked for": true,
-  //     "Feedback recieved": true,
-  //     "Mentor t-shirt returned": false,
-  //   },
-  // };
-
-  // const [isOnboardingChecked, setIsOnboardingChecked] = useState({
-  //   ...userProcess.user_onboarding_task,
-  // });
 
   return (
     <main
@@ -181,36 +116,6 @@ function ProfilePage() {
           <ProfileInformation />
         </div>
       </section>
-
-      {/* <section className="max-w-4xl mx-auto text-center border-t p-4 border-gray-300 mt-4">
-        <h2 className="font-bold text-xl pb-4">Mentor Onboarding Tasks</h2>
-        <div className="grid grid-rows-4 gap-4 text-center">
-          <div>
-            <ul>Slack username provided</ul>
-            {userProcess?.user_onboarding_task_slack && (
-              <CheckBadgeIcon className="w-6 h-6" />
-            )}
-          </div>
-          <div>
-            <ul>LinkedIn URL Provided</ul>
-            {userProcess?.user_onboarding_task_linkedin && (
-              <CheckBadgeIcon className="w-6 h-6" />
-            )}
-          </div>
-          <div>
-            <ul>Read the Code of Conduct</ul>
-            {userProcess?.user_onboarding_task_CodeofConduct && (
-              <CheckBadgeIcon className="w-6 h-6" />
-            )}
-          </div>
-          <div>
-            <ul>Mentor t-shirt Received</ul>
-            {userProcess?.onboard_process.user_onboarding_task_tshirtsent && (
-              <CheckBadgeIcon className="w-6 h-6" />
-            )}
-          </div>
-        </div>
-      </section> */}
 
       {/* bottom half - event cards */}
       <section className="w-full max-w-4xl text-center border-t p-4 border-gray-300 mt-4">
