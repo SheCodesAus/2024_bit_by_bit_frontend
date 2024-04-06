5f48e4e3854482d7d56b98443e4dfa3d461eb08f
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-06 19:27:28 +0800

    user ability to delete mentor availability.

--- src/components/UserComponents/Profile-EventCard.jsx
+++ src/components/UserComponents/Profile-EventCard.jsx
@@ -7,26 +7,28 @@ import { useAuth } from "../../hooks/use-auth";
 import ButtonElement from "../GlobalElements/Button";
 
 // API
-// import postCreateEventMentor from "../../api/post-create-eventMentor";
 import putUpdateEventMentor from "../../api/put-update-event-mentor";
 import getEventMentors from "../../api/get-event-mentors";
+import deleteMentor from "../../api/delete-eventMentor";
 
 // STYLE/TAILWIND
 import { Dialog, Transition } from "@headlessui/react";
-import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import {
+  HandThumbUpIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
 
 function ProfileEventCard({ eventData }) {
-  console.log("eventData inside profile card: ", eventData);
   const navigate = useNavigate();
   const { auth } = useAuth();
-  const [modalOpen, setModalOpen] = useState(false);
-  const cancelBtnRef = useRef(null);
-  // const eventLink = `/event/${eventData.id}`;
-  const [allEventMentors, setAllEventMentors] = useState();
-  const [allEventMentorsError, setAllEventMentorsError] = useState();
+
   const [newRoleRequested, setNewRoleRequested] = useState();
   const currentEvent = eventData.id;
+  const [mentorToManage, setMentorToManage] = useState();
 
+  // MENTOR RATIOS:
+  const [allEventMentors, setAllEventMentors] = useState();
+  const [allEventMentorsError, setAllEventMentorsError] = useState();
   useEffect(() => {
     getEventMentors(auth.token)
       .then((eventMentors) => {
@@ -37,7 +39,6 @@ function ProfileEventCard({ eventData }) {
       });
   }, []);
 
-  // MENTOR RATIOS:
   const mentorNumber = function (array) {
     if (array !== null && array !== undefined) {
       return array.filter((mentor) => mentor.event_id === currentEvent).length;
@@ -46,30 +47,7 @@ function ProfileEventCard({ eventData }) {
 
   const currentMentorNumber = mentorNumber(allEventMentors);
 
-  // const [eventMentorDetails, setEventMentorDetails] = useState({
-  //   event_id: eventData.id,
-  //   mentor_id: auth.user_id,
-  //   approved: false,
-  //   event_onboarding_task: {
-  //     event_contract: false,
-  //     slack_invite: false,
-  //     google_calendar_invite: false,
-  //     lms_invite: false,
-  //     mentor_bio: false,
-  //     mentor_photo: false,
-  //     create_mentor_img_asset: false,
-  //     reconfirm_dates: false,
-  //     building_information: false,
-  //   },
-  //   event_offboarding_task: {
-  //     invoice_sent: false,
-  //     feedback_recieved: false,
-  //   },
-  //   role_requested: "",
-  //   role_assigned: "Unassigned",
-  //   is_completed: false,
-  // });
-
+  // UPDATE AVAILABILITY
   const handleRoleRequestedChange = (event) => {
     const { id, value } = event.target;
     setNewRoleRequested((prevEventMentorDetails) => ({
@@ -91,14 +69,47 @@ function ProfileEventCard({ eventData }) {
     }
   };
 
-  // MODAL
+  // UPDATE MODAL
+  const cancelBtnRef = useRef(null);
+  const [modalOpen, setModalOpen] = useState(false);
   const modalTitle = "Availability Updated";
-  const modalMessage = `Thank you for updating you availability for our ${eventData.type}: ${eventData.name}, in ${eventData.location}. If you have any questions, please get in touch with Kate Kirwin.`;
+  const modalMessage = `Thank you for updating you availability for our ${eventData.type}: ${eventData.name}, in ${eventData.location} on ${eventData.date}. If you have any questions, please get in touch with Kate Kirwin.`;
   const modalBtnTxt = "Close";
+
+  const handleCancelBtnClick = (event) => {
+    setModalOpen(false);
+  };
 
   const handleModalBtnClick = async (event) => {
     setModalOpen(false);
     window.location.reload();
+  };
+
+  // DELETE AVAILABILITY
+  // DELETE MODAL
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const deleteModalTitle = "Availability Deleted";
+  const deleteModalMessage = `Are you sure you want to delete this mentor application for ${eventData.type}: ${eventData.name}, in ${eventData.location} on ${eventData.date}. All of the data will be permanently removed. This action cannot be undone. `;
+  const deleteModalBtnTxt = "Delete Availability";
+
+  const deleteMessage = "Delete Availability";
+  const deleteBtnClick = (id) => {
+    console.log("id in delete btn click: ", id);
+    setMentorToManage(id);
+    setDeleteModalOpen(true);
+  };
+
+  const handleDeleteModalBtnClick = async (id) => {
+    console.log("id in delete fnc: ", id);
+
+    const { data } = await deleteMentor({
+      mentor_id: id,
+      token: auth.token,
+    });
+    setDeleteModalOpen(false);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
   };
 
   return (
@@ -136,10 +147,14 @@ function ProfileEventCard({ eventData }) {
             message={availabilityBtnMessage}
             btnClick={handleSubmit}
           />
+          <ButtonElement
+            message={deleteMessage}
+            btnClick={() => deleteBtnClick(eventData.mentor_id)}
+          />
         </div>
       </div>
 
-      {/* SECTION  - Modal */}
+      {/* SECTION  - UPDATE MODAL */}
       <section>
         <Transition.Root show={modalOpen} as={Fragment}>
           <Dialog
@@ -201,6 +216,87 @@ function ProfileEventCard({ eventData }) {
                         onClick={handleModalBtnClick}
                       >
                         {modalBtnTxt}
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+      </section>
+      {/* SECTION  - DELETE MODAL */}
+      <section>
+        <Transition.Root show={deleteModalOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-10"
+            initialFocus={cancelBtnRef}
+            onClose={setDeleteModalOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                      <div className="sm:flex sm:items-start">
+                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <ExclamationTriangleIcon
+                            className="h-6 w-6 text-red-600"
+                            aria-hidden="true"
+                          />
+                        </div>
+                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                          <Dialog.Title
+                            as="h3"
+                            className="text-base font-semibold leading-6 text-gray-900"
+                          >
+                            {deleteModalTitle}
+                          </Dialog.Title>
+                          <div className="mt-2">
+                            <p className="text-sm text-gray-500">
+                              {deleteModalMessage}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md px-3 py-2 bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 sm:ml-3 sm:w-auto"
+                        onClick={() =>
+                          handleDeleteModalBtnClick(mentorToManage)
+                        }
+                      >
+                        {deleteModalBtnTxt}
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        onClick={handleCancelBtnClick}
+                        ref={cancelBtnRef}
+                      >
+                        Cancel
                       </button>
                     </div>
                   </Dialog.Panel>
