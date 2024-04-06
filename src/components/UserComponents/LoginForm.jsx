80ec0343b48dd430cb966acc45c6179069321610
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-06 14:33:29 +0800

    resolve admin reload bug

--- src/components/UserComponents/LoginForm.jsx
+++ src/components/UserComponents/LoginForm.jsx
@@ -74,6 +74,7 @@ function LoginForm() {
             is_admin: response.is_admin,
           });
           navigate("/home");
+          window.location.reload();
         })
         .catch((error) => {
           console.error("Login attempt failed: ", error);
