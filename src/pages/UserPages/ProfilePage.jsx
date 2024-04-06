86b718bc445a283023b03b22ed12b1803bdd118e
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-06 17:48:44 +0800

    working to get the event card on the profile pages

--- src/pages/UserPages/ProfilePage.jsx
+++ src/pages/UserPages/ProfilePage.jsx
@@ -12,7 +12,10 @@ import EventCard from "../../components/GlobalElements/EventCard";
 import { CheckBadgeIcon } from "@heroicons/react/24/solid";
 
 // API
+import getUser from "../../api/get-user.js";
+import getEvents from "../../api/get-events.js";
 import getUserProcess from "../../api/get-user-process.js";
+import getEventMentors from "../../api/get-event-mentors.js";
 
 function ProfilePage() {
   const { auth } = useAuth();
@@ -22,6 +25,18 @@ function ProfilePage() {
   console.log("user: ", user);
   const { isNavbarOpen } = useNavbarContext();
   const [selectedCity] = useState("");
+
+  const [userData, setUserData] = useState();
+  const [userError, setUserError] = useState();
+  useEffect(() => {
+    getUser(id)
+      .then((user) => {
+        setUserData(user);
+      })
+      .catch((error) => {
+        setUserError(error);
+      });
+  }, []);
 
   // Example events data
   const events = [
@@ -33,6 +48,80 @@ function ProfilePage() {
       time: "9am - 1pm",
     },
   ];
+
+  // REGISTERED EVENTS
+
+  const [eventMentors, setEventMentors] = useState();
+  const [mentorsError, setMentorsError] = useState();
+  useEffect(() => {
+    getEventMentors(id)
+      .then((mentors) => {
+        setEventMentors(mentors);
+      })
+      .catch((error) => {
+        setMentorsError(error);
+      });
+  }, []);
+
+  const [eventData, setEventData] = useState();
+  const [EventError, setEventError] = useState();
+  useEffect(() => {
+    getEvents()
+      .then((event) => {
+        setEventData(event);
+      })
+      .catch((error) => {
+        setEventError(error);
+      });
+  }, []);
+
+  // console.log("event mentors: ", eventMentors);
+  // console.log("user data: ", userData);
+  // console.log("event data: ", eventData);
+
+  let mentorApplications = [];
+  eventMentors &&
+    userData &&
+    eventMentors.map((mentor) => {
+      if (mentor.mentor_id == userData.id) {
+        const addApplication = {
+          id: mentor.id,
+          user_id: userData.id,
+          event_id: mentor.event_id,
+          role_requested: mentor.role_requested,
+          role_assigned: mentor.role_assigned,
+        };
+        mentorApplications.push(addApplication);
+      }
+    });
+
+  console.log("mentorApplications after push: ", mentorApplications);
+
+  let eventApplications = [];
+  mentorApplications.length > 0 &&
+    eventData &&
+    mentorApplications.map((mentor) => {
+      console.log("map step 1");
+      eventData.map((event) => {
+        console.log("map step 2");
+        if (mentor.id == event.id) {
+          console.log("map if fnc");
+          const addEventApplication = {
+            mentor_id: mentorApplications.id,
+            event_id: mentor.event_id,
+            name: event.event_name,
+            location: event.location,
+            date: event.event_start_date,
+            type: event.event_type,
+            role_requested: mentor.role_requested,
+            role_assigned: mentor.role_assigned,
+          };
+          eventApplications.push(addEventApplication);
+        }
+      });
+    });
+
+  console.log("eventApplications after push: ", eventApplications);
 
   const [userProcess, setUserProcess] = useState();
   const [processError, setProcessError] = useState();
@@ -50,8 +139,8 @@ function ProfilePage() {
 
   const filteredEvents = selectedCity
     ? events.filter(
-      (event) => event.location.toLowerCase() === selectedCity.toLowerCase()
-    )
+        (event) => event.location.toLowerCase() === selectedCity.toLowerCase()
+      )
     : events;
 
   // const userProcess = {
@@ -74,8 +163,9 @@ function ProfilePage() {
 
   return (
     <main
-      className={`min-h-screen ${isNavbarOpen ? "ml-60" : "ml-20"
-        } flex flex-col items-center`}
+      className={`min-h-screen ${
+        isNavbarOpen ? "ml-60" : "ml-20"
+      } flex flex-col items-center`}
     >
       <div className="flex justify-center p-4 border-gray-300 mb-8 pt-16">
         <h1 className="font-bold text-5xl ">hey there! </h1>
