21c0b5aa25baa47552272b82da44bf62bba4507f
caoimhejyoti <caoimhejyoti@gmail.com>
2024-04-06 17:43:12 +0800

    removing dead consolde logs

--- src/components/GlobalElements/NavBar.jsx
+++ src/components/GlobalElements/NavBar.jsx
@@ -43,8 +43,6 @@ function NavBar() {
     window.localStorage.removeItem("is_admin");
     setAuth({ token: null, user_id: null, username: null });
   };
-  console.log("auth", auth.is_admin);
-  console.log("storage", localStorage);
 
   return (
     <>
@@ -56,9 +54,14 @@ function NavBar() {
           <button
             className="flex items-center justify-start bg-orange-400 rounded-full p-1 shadow-sm"
             onClick={toggleNavbar}
-            style={{ marginRight: 'auto' }}
+            style={{ marginRight: "auto" }}
           >
-            <ChevronDoubleRightIcon alt="Toggle Navigation" className={`w-6 h-6 transition-transform duration-300 ${isNavbarOpen ? 'rotate-180' : ''}`} />
+            <ChevronDoubleRightIcon
+              alt="Toggle Navigation"
+              className={`w-6 h-6 transition-transform duration-300 ${
+                isNavbarOpen ? "rotate-180" : ""
+              }`}
+            />
           </button>
           <img id="text logo" src="/imgs/BTlogo.png" className="h-12" />
           <Link
@@ -74,22 +77,26 @@ function NavBar() {
         </section>
 
         <nav
-          className={`fixed top-12 h-[calc(100vh-3rem)] p-3 ${isNavbarOpen ? "w-60" : "w-20"
-            } text-white flex flex-col transition-all duration-500 ease-in-out`}
+          className={`fixed top-12 h-[calc(100vh-3rem)] p-3 ${
+            isNavbarOpen ? "w-60" : "w-20"
+          } text-white flex flex-col transition-all duration-500 ease-in-out`}
           style={{ backgroundColor: "rgb(130, 70, 175)" }}
         >
           <div
-            className={`flex flex-col items-center justify-center ${!isNavbarOpen ? "overflow-hidden" : ""}`}
+            className={`flex flex-col items-center justify-center ${
+              !isNavbarOpen ? "overflow-hidden" : ""
+            }`}
           >
             <a className="flex items-center justify-center">
               <img
                 src={logoPath}
                 alt="Logo"
-                className={`transition-all duration-300 ${isNavbarOpen ? "w-52 h-52" : "w-12 h-12"}`}
+                className={`transition-all duration-300 ${
+                  isNavbarOpen ? "w-52 h-52" : "w-12 h-12"
+                }`}
               />
             </a>
           </div>
-
 
           <div className="mt-auto flex flex-col justify-center items-center gap-2 pt-6 border-t border-orange-300">
             <Link
@@ -128,34 +135,35 @@ function NavBar() {
                 About
               </span>
             </Link>
-            {auth.token && 
-                auth.is_admin === 'true' && (
-                  <Link
-                    to="/users/manage"
-                    className="link flex items-center gap-4 p-2 rounded-md hover:bg-orange-600"
-                  >
-                    <CogIcon alt="logout" className="w-7 h-7" />
-                    <span className={`${isNavbarOpen ? "inline" : "hidden"}`}>
-                      Users
-                    </span>
-                  </Link>
-                )
-                }
+            {auth.token && auth.is_admin === "true" && (
+              <Link
+                to="/users/manage"
+                className="link flex items-center gap-4 p-2 rounded-md hover:bg-orange-600"
+              >
+                <CogIcon alt="logout" className="w-7 h-7" />
+                <span className={`${isNavbarOpen ? "inline" : "hidden"}`}>
+                  Users
+                </span>
+              </Link>
+            )}
             <div className="user-profile flex justify-center items-center gap-4 pt-6">
               <div className="user-avatar w-12 h-12">
-                <Link
-                  to={`/users/${auth.user_id}`}
-                >
-                  <UserCircleIcon alt="user" className="w-full h-full rounded-full" />
+                <Link to={`/users/${auth.user_id}`}>
+                  <UserCircleIcon
+                    alt="user"
+                    className="w-full h-full rounded-full"
+                  />
                 </Link>
               </div>
               <div
-                className={`user-details ${isNavbarOpen ? "inline" : "hidden"} text-center`}
+                className={`user-details ${
+                  isNavbarOpen ? "inline" : "hidden"
+                } text-center`}
               >
-                <Link
-                  to={`/users/${auth.user_id}`}
-                >
-                  <p className="username font-semibold">{user?.first_name} <br /> {user?.last_name}</p>
+                <Link to={`/users/${auth.user_id}`}>
+                  <p className="username font-semibold">
+                    {user?.first_name} <br /> {user?.last_name}
+                  </p>
                 </Link>
               </div>
             </div>
